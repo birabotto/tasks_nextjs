36133050a4acfbb48669d56e50b7c8bac10efbd1
Bira Botto <tibira.botto@gmail.com>
2024-08-24 14:23:18 -0400

    fix error module

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../../styles/globals.css";
+import "@/styles/Home.module.css";
 import type { AppProps } from "next/app";
 import { Header } from "../components/header";
 import { SessionProvider } from "next-auth/react";
